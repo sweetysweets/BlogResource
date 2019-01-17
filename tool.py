04854b555e6b67f46f6f3f13b1c0932116768050
sweets <141250073@smail.nju.edu.cn>
2019-01-17 13:07:25 +0800

    add photos

--- tool.py
+++ tool.py
@@ -89,53 +89,98 @@ def compress_photo():
             file_list_src.remove(file_list_des[i])
     compress('4', des_dir, src_dir, file_list_src)
 
+#
+#def handle_photo():
+#    '''根据图片的文件名处理成需要的json格式的数据
+#
+#    -----------
+#    最后将data.json文件存到博客的source/photos文件夹下
+#    '''
+#    src_dir, des_dir = "photos/", "min_photos/"
+#    file_list = list_img_file(src_dir)
+#    list_info = []
+#    date_list = []
+#    for i in range(len(file_list)):
+#        filename = file_list[i]
+#        date_str, info = filename.split("_")
+#        info, _ = info.split(".")
+#        date = datetime.strptime(date_str, "%Y-%m-%d")
+#        year_month = '-'.join(date_str.split('-')[0:2])
+#        if i == 0:  # 处理第一个文件
+#            new_dict = {"date": year_month, "arr": {'year': date.year,
+#                                                    'month': date.month,
+#                                                    'link': [filename],
+#                                                    'text': [info],
+#                                                    'type': ['image']
+#                                                    }
+#                        }
+#            date_list.append(date)
+#            list_info.append(new_dict)
+#        elif date in date_list:  # 是同一个日期,找到在 Date 数组中的索引
+#            index = date_list.index(date)
+#            list_info[index]['arr']['link'].append(filename)
+#            list_info[index]['arr']['text'].append(info)
+#            list_info[index]['arr']['type'].append('image')
+#
+#        else:  # 不是同一个日期创建一个新的 dict
+#            new_dict = {"date": year_month, "arr":{'year': date.year,
+#                                                   'month': date.month,
+#                                                   'link': [filename],
+#                                                   'text': [info],
+#                                                   'type': ['image']
+#                                                   }
+#                        }
+#            date_list.append(date)
+#            list_info.append(new_dict)
+#
+#    list_info = SortDict(list_info)
+#    final_dict = {"list": list_info}
+#    with open("../blog/source/photos/data.json", "w") as fp:
+#        json.dump(final_dict, fp)
+#
+
 
 def handle_photo():
     '''根据图片的文件名处理成需要的json格式的数据
-
-    -----------
-    最后将data.json文件存到博客的source/photos文件夹下
-    '''
+        
+        -----------
+        最后将data.json文件存到博客的source/photos文件夹下
+        '''
     src_dir, des_dir = "photos/", "min_photos/"
     file_list = list_img_file(src_dir)
     list_info = []
-    date_list = []
+    file_list.sort(key=lambda x: x.split('_')[0])   # 按照日期排序
     for i in range(len(file_list)):
         filename = file_list[i]
         date_str, info = filename.split("_")
         info, _ = info.split(".")
         date = datetime.strptime(date_str, "%Y-%m-%d")
-        year_month = '-'.join(date_str.split('-')[0:2])
+        year_month = date_str[0:7]
         if i == 0:  # 处理第一个文件
-            new_dict = {"date": year_month, "arr": {'year': date.year,
-                                                    'month': date.month,
-                                                    'link': [filename],
-                                                    'text': [info],
-                                                    'type': ['image']
-                                                    }
-                        }
-            date_list.append(date)
-            list_info.append(new_dict)
-        elif date in date_list:  # 是同一个日期,找到在 Date 数组中的索引
-            index = date_list.index(date)
-            list_info[index]['arr']['link'].append(filename)
-            list_info[index]['arr']['text'].append(info)
-            list_info[index]['arr']['type'].append('image')
-            
-        else:  # 不是同一个日期创建一个新的 dict
             new_dict = {"date": year_month, "arr":{'year': date.year,
-                                                   'month': date.month,
-                                                   'link': [filename],
-                                                   'text': [info],
-                                                   'type': ['image']
-                                                   }
-                        }
-            date_list.append(date)
+                'month': date.month,
+                    'link': [filename],
+                        'text': [info],
+                            'type': ['image']
+                            }
+                                }
             list_info.append(new_dict)
-
-    list_info = SortDict(list_info)
+        elif year_month != list_info[-1]['date']:  # 不是最后的一个日期，就新建一个dict
+            new_dict = {"date": year_month, "arr":{'year': date.year,
+                'month': date.month,
+                    'link': [filename],
+                        'text': [info],
+                            'type': ['image']
+                            }
+                    }
+            list_info.append(new_dict)
+        else:  # 同一个日期
+            list_info[-1]['arr']['link'].append(filename)
+            list_info[-1]['arr']['text'].append(info)
+            list_info[-1]['arr']['type'].append('image')
+    list_info.reverse()  # 翻转
     final_dict = {"list": list_info}
-    with open("../blog/source/photos/data.json", "w") as fp:
+    with open("../blog/source/photos/data.json","w") as fp:
         json.dump(final_dict, fp)
 
 
